475ed2133596929e56cf2ec69218050c72dcaadf
gerald91220 <dg.andriana@gmail.com>
2015-03-19 11:53:19 +0100

    header et footer

--- public/Tache/index.php
+++ public/Tache/index.php
@@ -5,61 +5,27 @@ use tache\Controller\TacheController;
 $taches = new TacheController();
 $listesTaches = $taches->ListesTaches();
 $taches->postTache($_GET);
-
+require_once '../template/header.php';
 ?>
-
-<!DOCTYPE html>
-<html lang="">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="../favicon.ico">
-
-    <title>Dashboard Template for Bootstrap</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="../dist/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="../css/dashboard.css" rel="stylesheet">
-
-    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
-    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-    <script src="../assets/js/ie-emulation-modes-warning.js"></script>
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
-
-  <body>
-
     <div class="container-fluid">
       <div class="row">
         <div class="col-sm-3 col-md-2 ">
 
         </div>
         <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-          <h1 class="page-header">Dashboard</h1>
-
           <h2 class="sub-header">Tableau de bord</h2>
           <div class="table-responsive">
             <table class="table table-striped">
               <thead>
                 <tr>
                   <th>#</th>
-                  <th>Tâche</th>
+                  <th>Titre</th>
                   <th>description</th>
                   <th>Echeance</th>
-                  <th>Avancement par utilisateur </th>
-                  <th>Avancement</th>
+                  <th>Temps de réalisation <br>par utilisateur </th>
+                  <th>Temps de réalisation <br>Total</th>
                   <th>Etat en Temps</th>
-                  <th>Etat</th>
+                  <th>Etat du tache</th>
                   <th>Affectation</th>
                   <th></th>
                 </tr>
@@ -88,14 +54,4 @@ $taches->postTache($_GET);
         </div>
       </div>
     </div>
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-    <script src="../../dist/js/bootstrap.min.js"></script>
-    <script src="../../assets/js/docs.min.js"></script>
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
-  </body>
-</html>
+<?php require_once '../template/footer.php'; ?>
